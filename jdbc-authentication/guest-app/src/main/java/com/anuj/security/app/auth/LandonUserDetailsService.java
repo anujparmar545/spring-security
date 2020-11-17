22bddf72c5a24b9c420ff81533dd417f1dc40847
Anuj Parmar <anuj.parmar@fisglobal.com>
2020-11-17 18:14:42 +0530

    second commit

---
+++ jdbc-authentication/guest-app/src/main/java/com/anuj/security/app/auth/LandonUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.anuj.security.app.auth;
+
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LandonUserDetailsService  implements UserDetailsService{
+
+    private final UserRepository userRepository;
+    private final AuthGroupRepository authGroupRepository;
+
+    public LandonUserDetailsService(UserRepository userRepository, AuthGroupRepository authGroupRepository){
+        super();
+        this.userRepository = userRepository;
+        this.authGroupRepository = authGroupRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if(null==user){
+            throw new UsernameNotFoundException("cannot find username: " + username);
+        }
+        List<AuthGroup> authGroups = this.authGroupRepository.findByUsername(username);
+        return new LandonUserPrincipal(user, authGroups);
+    }
+    
+}
